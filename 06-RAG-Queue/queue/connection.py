b6f5ca159d4cd5ab5f44653b0ffd504ca0734cc5
Utkarsh <93428291+utkarsh240@users.noreply.github.com>
2025-06-19 17:05:15 +0530

    update docker

---
+++ 06-RAG-Queue/queue/connection.py
@@ -0,0 +1,6 @@
+from redis import Redis
+from rq import Queue
+
+queue = Queue(connection=Redis())
+
+
--- 06-RAG-Queue/server.py
+++ 06-RAG-Queue/server.py
@@ -1,11 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
+from .queue.connection import queue
+from .queue.worker import process_query
 
 app = FastAPI()
 
 @app.get('/')
-def chat():
+def root():
     return {"status: Server is up and running"}
 
 @app.post('/chat')
-def chat():
-    pass
+def chat(
+    query: str  = Query(..., description = "Chat message")
+):
+   job = queue.enqueue(process_query, query)
+   return {"status": "queued", "job_id": job.id}
