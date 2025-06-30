13d8fc69094b9a35c84eadc4f573e125c5aeb42f
Utkarsh <93428291+utkarsh240@users.noreply.github.com>
2025-06-30 16:38:31 +0530

    .

---
+++ chat_graph/chat.py
@@ -0,0 +1,23 @@
+from typing_extensions import TypedDict
+from typing import Annotated
+from langgraph.graph.message import add_messages
+from langchain.chat_models import init_chat_model
+from langgraph.graph import StateGraph, START, END
+
+class State(TypedDict):
+    messages: Annotated[list, add_messages]
+    
+llm = init_chat_model(model_provider="openai", model="gpt-4.1")
+
+    
+def chat_node(state: State):
+    response =  llm.invoke(state["messages"])
+    return { "messages": [response]}
+
+
+graph_builder = StateGraph(State)
+
+graph_builder.add_node("chat_node", chat_node)
+
+graph_builder.add_edge(START, "chat_node")
+graph_builder.add_edge("chat_node", END)
