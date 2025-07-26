5a8794a7b729cd21640c4a17d76aff486515027e
Utkarsh <93428291+utkarsh240@users.noreply.github.com>
2025-07-26 23:00:40 +0530

    .

--- chat_graph/chat.py
+++ chat_graph/chat.py
@@ -3,16 +3,17 @@ from typing import Annotated
 from langgraph.graph.message import add_messages
 from langchain.chat_models import init_chat_model
 from langgraph.graph import StateGraph, START, END
+from dotenv import load_dotenv
+load_dotenv()
 
 class State(TypedDict):
     messages: Annotated[list, add_messages]
-    
 llm = init_chat_model(model_provider="openai", model="gpt-4.1")
 
     
 def chat_node(state: State):
-    response =  llm.invoke(state["messages"])
-    return { "messages": [response]}
+    response = llm.invoke(state["messages"])
+    return {"messages": [response]}
 
 
 graph_builder = StateGraph(State)
@@ -21,3 +22,13 @@ graph_builder.add_node("chat_node", chat_node)
 
 graph_builder.add_edge(START, "chat_node")
 graph_builder.add_edge("chat_node", END)
+
+graph = graph_builder.compile()
+
+def main():
+    query = input(">")
+    
+    result = graph.invoke({"messages": [{"role": "user", "content": query}]})
+    print(result)
+    
+main()
